2a9ac978cbd8a29dedf84dc98431378f29a92bac
Jose Manuel <jotaemegecrespo+codetest@gmail.com>
2018-04-03 12:53:40 +0100

    feat: add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect('/comics')
   get '/comics/', to: 'comics#index'
   post '/comics/:id/fav', as: :comic_fav, to: 'comics#fav'
 end
