735cfeb804b8b56a77efcdf49bdcc502b1f56bed
Jose Manuel <jmgcrespo+codetest@gmail.com>
2018-04-03 11:44:02 +0100

    fix: empty results for character not found

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -2,8 +2,10 @@ class ComicsController < ApplicationController
   def index
     if @character = params[:character]
       @characters = Marvel::CharacterSearch.new(character: params[:character]).characters
+      render and return unless @characters.present?
     end
     @search = Marvel::ComicsSearch.new(page: params[:page], characters: @characters&.map(&:id))
+
   end
 
   def fav
